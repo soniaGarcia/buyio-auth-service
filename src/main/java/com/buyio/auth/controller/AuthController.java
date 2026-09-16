8c381fabe2e66a33cc7e63836f7fa7d7d324d69f
Sonia Guadalupe Garcia <sonia.guadalupe.garcia@gmail.com>
2026-09-16 11:47:04 -0600

    Fix path

--- src/main/java/com/buyio/auth/controller/AuthController.java
+++ src/main/java/com/buyio/auth/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
