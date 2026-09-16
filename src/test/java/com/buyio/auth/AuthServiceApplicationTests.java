05e4d63c5e304f1fc7f0a70d34c6b851a48ddb08
Sonia Guadalupe Garcia <sonia.guadalupe.garcia@gmail.com>
2026-09-16 08:18:24 -0600

    Fix

--- src/test/java/com/buyio/auth/AuthServiceApplicationTests.java
+++ src/test/java/com/buyio/auth/AuthServiceApplicationTests.java
@@ -2,9 +2,17 @@ package com.buyio.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean; 
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
+
+	@MockitoBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
 	@Test
 	void contextLoads() {
